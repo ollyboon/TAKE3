57d6e61ccbc68aef14688baeb4d70e76eaba8319
Amy Wichelow <i7683807@bournemouth.ac.uk>
2016-05-24 16:17:42 +0100

    Resolution of map

--- Folding/Route.swift
+++ Folding/Route.swift
@@ -39,7 +39,7 @@ func buildRoutes() -> [Route] {
         duration: "25 minutes",
         info: "Take a journey through the estate's varied environments",
         terrainInfo: "Variable path surface, some epic areas",
-        terrainImage: "heathlandterrain",
+        terrainImage: "heathlandterrain@2",
         mapRouteImage: "mapRouteImage5",
         color: UIColor(redX: 241, greenX: 95, blueX: 140, alphaX: 1),
         colorDiff: "red"
@@ -51,7 +51,7 @@ func buildRoutes() -> [Route] {
         duration: "15 minutes",
         info: "Stimulating the senses, taking you through a variety of habitats",
         terrainInfo: "Variable path surface, some grassed areas",
-        terrainImage: "sensoryterrain",
+        terrainImage: "sensoryterrain@2",
         mapRouteImage: "mapRouteImage1",
         color: UIColor(redX: 96, greenX: 142, blueX: 230, alphaX: 1),
         colorDiff: "green"
@@ -62,7 +62,7 @@ func buildRoutes() -> [Route] {
         duration: "5 minutes",
         info: "Take a journey to the Bird hide, an ideal place to view the varied bird species of Holton Lee",
         terrainInfo: "Variable path surface, some wet areas",
-        terrainImage: "birdhideterrain",
+        terrainImage: "birdhideterrain@2",
         mapRouteImage: "mapRouteImage2",
         color: UIColor(redX: 89, greenX: 137, blueX: 35, alphaX: 1),
         colorDiff: "green"
@@ -73,7 +73,7 @@ func buildRoutes() -> [Route] {
         duration: "10 minutes",
         info: "Enjoy a walk to the Clump, with stunning views and our impressive weaving tree",
         terrainInfo: "Variable path surface, some dry areas",
-        terrainImage: "clumpterrain",
+        terrainImage: "clumpterrain@2",
         mapRouteImage: "mapRouteImage3",
         color: UIColor(redX: 225, greenX: 110, blueX: 110, alphaX: 1),
         colorDiff: "orange"
@@ -84,7 +84,7 @@ func buildRoutes() -> [Route] {
         duration: "15 minutes",
         info: "A trail leading you to the sea, with a great vantage point",
         terrainInfo: "Variable path surface, some muddy areas",
-        terrainImage: "seaviewterrain",
+        terrainImage: "seaviewterrain@2",
         mapRouteImage: "mapRouteImage4",
         color: UIColor(redX: 216, greenX: 132, blueX: 253, alphaX: 1),
         colorDiff: "orange"
